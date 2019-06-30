e553495bfa9f5b546ed2b7447f1cc988834e9263
xiaohuihuiovo <1666352306@qq.com>
2019-06-30 20:59:30 +0800

    继续完善了一些功能

--- mygit/js/index.js
+++ mygit/js/index.js
@@ -4,9 +4,9 @@ $(function() {
   var btn1 = document.querySelector('.btn-danger')
   var btn2 = document.querySelector('.btn-info')
 
-
-  function timer() {
-    //设置定时器
+  one.addEventListener('click', function(e) {
+    window.confirm('想什么呢¿ 醒醒吧 ');
+    // 设置定时器
     var time = 5;
     var timer = setInterval(function() {
       if (time == 0) {
@@ -15,20 +15,16 @@ $(function() {
         second.innerHTML = '还剩' + time + '秒后跳转到秘诀';
         time--;
       }
+      btn1.addEventListener('click', function(e) {
+        clearInterval(timer);
+      })
     }, 1000);
-  }
+  });
 
-  one.addEventListener('click', function(e) {
-    window.confirm('想什么呢¿ 醒醒吧 ')
-    timer();
-  });
-  // location.href = 'https://www.jianshu.com/p/6f95ae440fbb';
-  btn1.addEventListener('click', function(e) {
-    clearInterval(timer());
-  })
-  btn2.addEventListener('click', function(e) {
-    timer();
-  });
+
+  // btn2.addEventListener('click', function(e) {
+  //   timer();
+  // });
 
 
 

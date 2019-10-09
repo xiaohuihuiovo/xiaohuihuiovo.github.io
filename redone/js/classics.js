ae9d23749eab3126f581028d83b9a41eea1e4033
xiaohuihuiovo <1666352306@qq.com>
2019-10-09 22:25:51 +0800

    1

--- redone/js/classics.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-  $('.right').children('a').click(function() {
-    $(this).addClass('arrow ').siblings().removeClass();
-  })
-})
--- redone/js/common.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  // var a = document.querySelector('#nav').querySelectorAll(a);
-
-  // var nav = document.getElementById('nav');
-  // var a = nav.getElementsByTagName(a)
-  $('#nav').children('a').click(function() {
-    $(this).addClass('baseline').siblings().removeClass();
-  })
-})
